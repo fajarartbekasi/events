eb28e2b33b3f0f0df9e7bdba9fa0f087abef9792
fajarartbekasi <fajarbekasieditting@gmail.com>
2020-07-27 23:26:45 +0700

    create migration

---
+++ database/migrations/2020_07_27_154341_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('nisn');
+            $table->string('room');
+            $table->string('major');
+            $table->string('phone_number');
+            $table->string('address');
+            $table->string('gender');
+            $table->string('religion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
